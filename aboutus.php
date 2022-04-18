a498a16979518326a13193005d28ebe71c318c8f
Snehal Patil <88209483+snehapatil12@users.noreply.github.com>
2022-04-18 23:25:19 +0530

    add files

---
+++ aboutus.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+</head>
+<body>
+ 
+<a href="index.php"><button type="button1" class="btn btn-primary btn-lg mb-3"> Back to Home</button></a>
+
+</body>
+</html>
+<img src="background/aboutus.png" width="1380" height="650" title="About Us" alt="..." />
+
